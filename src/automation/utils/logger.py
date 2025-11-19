9eb7948f24a81710d568c6672836ce2f037a88ef
martinhoward4468-blip <martinhoward4468@gutkai.com>
2025-11-19 04:44:52 -0600

    feat: add src/automation/utils/logger.py

---
+++ src/automation/utils/logger.py
@@ -0,0 +1,11 @@
+thonimport logging
+
+def get_logger(name):
+ logger = logging.getLogger(name)
+ if not logger.handlers:
+ handler = logging.StreamHandler()
+ fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ handler.setFormatter(fmt)
+ logger.addHandler(handler)
+ logger.setLevel(logging.INFO)
+ return logger
